63dcaf876d65740b4fd7a707112daaffaa49b97d
vbadkar <vivekbadkar@gmail.com>
2022-01-05 07:54:58 +0530

    #14 Added hamburger icon and adjusted the header

--- web/themes/custom/the_wire/js/hamburger.js
+++ web/themes/custom/the_wire/js/hamburger.js
@@ -1,8 +1,8 @@
 const hamburger = document.querySelector('#Layer_1');
 const list = document.querySelector('.nav-bar');
-// const icon = document.querySelector('.close');
+hamburger.classList.add('show');
 hamburger.addEventListener('click', function(){
     list.classList.toggle('show');
+    hamburger.classList.remove('show');
     hamburger.classList.toggle('hide');
-    // icon.classList.toggle('show');
 });
---
+++ web/themes/custom/the_wire/js/scrollNav.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function(){
+  let navBar = document.querySelector('.site-header');
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      navBar.classList.add("scroll");
+  }
+  else{
+      navBar.classList.remove("scroll");
+  }
+})
