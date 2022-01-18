2dffdf175ed42f3be2b257ce81638979c6950d5f
vbadkar <vivekbadkar@gmail.com>
2022-01-18 18:03:49 +0530

    Removed the browser sync from gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,12 @@ const browserSync = require('browser-sync').create();
    //3. where do i save the compiled css?
    .pipe(gulp.dest('/var/www/html/the_wire/web/themes/custom/the_wire/css'))
    .pipe(browserSync.stream());
- }
 
+ }
+   gulp.task('watch', async function() {
+    gulp.watch('./assets/sass/*.scss', gulp.series('sass'));
+  });
  function watch(){
-   browserSync.init({
-     server: {
-       baseDir: '/var/www/html/the_wire/web/themes/custom/the_wire/sass'
-     }
-   });
    gulp.watch('/var/www/html/the_wire/web/themes/custom/the_wire/sass/**/*.scss', style)
    gulp.watch('/var/www/html/the_wire/web/themes/custom/the_wire/*.html').on('change', browserSync.reload);
    gulp.watch('/var/www/html/the_wire/web/themes/custom/the_wire/js/**/*.js')
